288a384d7c96d03cc2e1ceea12deee97024c60c9
Pavel Gajdůšek <pavel@gajdusek.eu>
2022-05-08 10:58:15 +0200

    Initial version

--- BlazorServer/Hubs/ChatHub.cs
+++ BlazorServer/Hubs/ChatHub.cs
@@ -1,6 +1,11 @@
+using Microsoft.AspNetCore.SignalR;
 namespace BlazorServer.Hubs;
 
-public class ChatHub
+public class ChatHub : Hub
 {
-    
+
+    public Task SendMessage(string user, string message)
+    {
+        return Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
 }
--- BlazorServer/Program.cs
+++ BlazorServer/Program.cs
@@ -1,6 +1,9 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
+
 using BlazorServer.Data;
+using Microsoft.AspNetCore.ResponseCompression;
+using BlazorServer.Hubs;
+
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +11,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddResponseCompression(opts =>
+{
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[] {"application/octet-stream"});
+});
 
 var app = builder.Build();
 
@@ -26,6 +33,7 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.MapBlazorHub();
+app.MapHub<ChatHub>("/chathub");
 app.MapFallbackToPage("/_Host");
 
 app.Run();
